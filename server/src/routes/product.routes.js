f470dbb0a986659647537661ff34314a75ac07c0
AkashMondal27 <akashmondal102003@gmail.com>
2026-08-20 20:03:33 +0530

    feat: add getSingleProduct controller for retrieving a single product by ID

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -4,10 +4,15 @@ import { isAuth } from "../middlwares/isAuth.middleware.js";
 import { createProduct } from "../controllers/product.controller.js";
 import { uploadFiles } from "../middlwares/multer.middleware.js";
 import { getAllProducts } from "../controllers/getAllProduct.controller.js";
-
+import { getSingleProduct } from "../controllers/getSingleProduct.controller.js";
 
 const router = express.Router();
 
 router.route("/new").post(isAuth,uploadFiles,  createProduct)
 router.route("/all").get(getAllProducts)
+router.route("/:id").get(getSingleProduct)
+
+
+
+
 export default router ;
