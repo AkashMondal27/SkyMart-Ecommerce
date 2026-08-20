b3c961fe8de5d7281b7d0b1b91a51af03b27f01c
AkashMondal27 <akashmondal102003@gmail.com>
2026-08-21 03:06:46 +0530

    feat: add order model and routes for order management

--- server/src/app.js
+++ server/src/app.js
@@ -13,6 +13,7 @@ import userRouter from "./routes/user.routes.js";
 import  productRoutes  from "./routes/product.routes.js";
 import cartRoutes from "./routes/cart.routes.js"
 import adddessRoutes from "./routes/address.routes.js"
+import orderRoutes from "./routes/order.routes.js";
 
 
 // Routes declaration
@@ -20,6 +21,8 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products" ,productRoutes);
 app.use("/api/v1/cart" , cartRoutes)
 app.use("/api/v1/address" , adddessRoutes)
+app.use("/api/v1/order" , orderRoutes)
+
 
 
 app.get("/", (req, res) => {
---
+++ server/src/models/order.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import { Product } from "./product.model.js";
+import User from "./user.model";
+
+const orderSchema=new mongoose.Schema({
+    items:[{
+        quantity :{
+            type :Number,
+            required :true
+        },
+        product :{
+            type :mongoose.Schema.Types.ObjectId,
+            ref:"Product",
+            required :true
+        }
+    }],
+    method :{
+        type: String,
+        required:true,
+    },
+    paymentInfo:{
+        type:String,
+    },
+    user :{
+            type :mongoose.Schema.Types.ObjectId,
+            ref:"User",
+            required :true
+        },
+    phone:{
+        type:Number,
+        required: true
+    }  ,
+    address:{
+        type:String,
+        required : true
+    }  ,
+    paidAt:{
+        type:String
+    },
+    subTotal:{
+        type:Number,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default :Date.now
+    }
+
+} ,{timestamps:true})
+
+export const Order= mongoose.model("Order" , orderSchema)
