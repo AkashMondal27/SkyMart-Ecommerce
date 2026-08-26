4b4a8123f61d3841ff6b67dfc60f39495438b0de
AkashMondal27 <akashmondal102003@gmail.com>
2026-08-26 12:24:14 +0530

    complete the  navbar section and starting the foote section

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <div>
-      <h1>this is footer section </h1>
-    </div>
-  )
+    return (
+
+        <footer className='w-full mt-8'>
+            <hr className='border-gray-300 dark:border-gray-700' />
+            <div className='container mx-auto px-4 py-6'>
+                <div className=' flex flex-col md:flex-row justify-between items-center'>
+                    <div className='mb-6 flex flex-col md:flex-row justify-between items-center'>
+                        <h1 className=' text-xl font-bold'>SkyMart</h1> 
+                        <p className=' text-sm '>Everything you need , all in one place .</p>
+                    </div>
+
+                    <div className=' flex flex-wrap justify-center md:justify-end gap-4'>
+                        <Link to="#" className="text-sm hover:underline">
+                            About Us
+                        </Link>
+
+                        <Link to="#" className="text-sm hover:underline">
+                            Contact Us
+                        </Link>
+
+                        <Link to="#" className="text-sm hover:underline">
+                            Term and Condition
+                        </Link>
+                    </div>
+                </div>
+
+                {/* ADD social Links */}
+                <div className='mt-6 text-center'>
+                    <p className='text-sm'>Follow us: </p>
+                    <div className='flex justify-center gap-4 mt-2'>
+                        <a href='#' className=' hover:opacity-75'>
+                            facebook
+                        </a>
+                        
+                    </div>
+                </div>
+            
+            </div>
+        </footer>
+
+    )
 }
 
 export default Footer
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import { LogIn, ShoppingCart, User } from 'lucide-react';
+import { Home as HomeIcon, ShoppingBag, LogIn, ShoppingCart, User } from 'lucide-react';
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuGroup, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu';
 
 const Navbar = () => {
 
     const navigate = useNavigate();
+    const location = useLocation();
 
     const logoutHandler = () => {
         alert("Logged Out")
@@ -14,32 +15,88 @@ const Navbar = () => {
     const isAuth = true;
 
     return (
-        <div className='z-50 sticky top-0 bg-background/50 border-b backdrop:blur'>
-            <div className=' container mx-auto px-6 py-4 flex flex-col sm:flex-row items-center justify-between'>
-                <h1 className=' text-2xl font-bold'> SkyMart</h1>
-                <ul className=' flex justify-center items-center space-x-6 '>
-                    <li className=' cursor-pointer' onClick={() => navigate("/")}>
-                        Home
+        <div className='z-50 sticky top-0 bg-zinc-200/50   border-b backdrop:blur '>
+            {/* <div className='container mx-auto px-6 py-4 flex flex-col sm:flex-row items-center justify-between'> */}
+            <div className="container mx-auto px-6 py-4 flex items-center justify-between">
+
+                {/*  SkyMart Logo */}
+                <h1 className=' text-2xl font-bold '> <span className='text-blue-400'>Sky</span>Mart</h1>
+
+                {/* <ul className=' flex justify-center items-center space-x-6 '> */}
+                <ul
+                    className="
+                fixed bottom-0 left-0 right-0
+                md:static
+                flex justify-around md:justify-center
+                items-center
+                space-x-0 md:space-x-6
+                backdrop-blur
+                border-t md:border-0
+                 bg-zinc-200/50 md:bg-transparent 
+                py-3 md:py-0
+                z-50
+                "
+                >
+
+                    {/* Home */}
+                    <li onClick={() => navigate("/")}
+                        className={` w-auto md:w-16 cursor-pointer flex items-center gap-1 transition-colors duration-200
+                            ${location.pathname === "/"
+                                ? "text-orange-500 font-bold"
+                                : "text-gray-700 hover:text-orange-500 font-semibold"
+                            } 
+                             `}
+
+                    >
+                        {/* Mobile */}
+                        <HomeIcon className="w-5 h-5 md:hidden " strokeWidth={location.pathname === "/" ? 3 : 2} />
+
+                        {/* Medium & Large */}
+                        <span className="hidden md:inline">
+                            Home
+                        </span>
                     </li>
 
-                    <li className=' cursor-pointer' onClick={() => navigate("/products")}>
-                        Products
+
+
+                    {/* Products */}
+                    <li
+                        onClick={() => navigate("/products")}
+                        className={`w-auto md:w-24 cursor-pointer flex items-center gap-1 transition-colors duration-200
+                            ${location.pathname === "/products"
+                                ? "text-orange-500 font-bold"
+                                : "text-gray-700 hover:text-orange-500 font-semibold"
+                            }`}
+                    >
+                        <ShoppingBag className="w-5 h-5 md:hidden " strokeWidth={location.pathname === "/products" ? 3 : 2} />
+                        <span className="hidden md:inline">Products</span>
                     </li>
 
-                    <li className=' cursor-pointer relative flex items-center' onClick={() => navigate("/cart")}>
-                        <ShoppingCart className='w-6 h-6' />
-                        <span className=' absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold
-                        w-5 h-5 flex items-center justify-center rounded-full'>
+
+                    {/* Cart */}
+                    <li
+                        onClick={() => navigate("/cart")}
+                        className={`w-auto md:w-12 cursor-pointer relative flex items-center transition-colors duration-200
+                            ${location.pathname === "/cart"
+                                ? "text-orange-500 font-bold"
+                                : "text-gray-700 hover:text-orange-500 font-semibold"
+                            }`}
+                    >
+                        <ShoppingCart className="w-5 h-5" strokeWidth={location.pathname === "/cart" ? 3 : 2} />
+
+                        <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs font-bold w-4 h-4 flex items-center justify-center rounded-full">
                             5
                         </span>
                     </li>
 
+
+
                     <li className='cursor-pointer'>
                         <DropdownMenu>
                             <DropdownMenuTrigger
-                                className="inline-flex h-9 w-9 items-center justify-center rounded-md cursor-pointer hover:bg-muted"
+                                className="inline-flex h-9 w-9 items-center justify-center rounded-md cursor-pointer transition-colors duration-200  hover:text-orange-500 hover:bg-orange-50"
                             >
-                                {isAuth ? <User className="h-5 w-5" /> : <LogIn className="h-5 w-5" />}
+                                {isAuth ? <User className="h-5 w-5 cursor-pointer" /> : <LogIn className="h-6 w-6 cursor-pointer" />}
                             </DropdownMenuTrigger>
 
                             <DropdownMenuContent align="end">
@@ -52,16 +109,16 @@ const Navbar = () => {
                                     <DropdownMenuSeparator />
 
                                     {!isAuth ? (
-                                        <DropdownMenuItem onClick={() => navigate("/login")}>
+                                        <DropdownMenuItem className="cursor-pointer" onClick={() => navigate("/login")}>
                                             Login
                                         </DropdownMenuItem>
                                     ) : (
                                         <>
-                                            <DropdownMenuItem onClick={() => navigate("/order")}>
+                                            <DropdownMenuItem className="cursor-pointer" onClick={() => navigate("/order")}>
                                                 Your Order
                                             </DropdownMenuItem>
 
-                                            <DropdownMenuItem onClick={logoutHandler}>
+                                            <DropdownMenuItem className="cursor-pointer" onClick={logoutHandler}>
                                                 Logout
                                             </DropdownMenuItem>
                                         </>
